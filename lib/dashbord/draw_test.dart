580cad9a2ccdd3be623c85adb25f40e1aa7ad90f
AtumaKen <atumaken@gmail.comgit config --global user.name AtumaKengit commitgit statusgit statusgitgit config --global user.email atumaken@gmail.com>
2020-11-04 14:32:50 +0100

    before starting screens

--- lib/dashbord/draw_test.dart
+++ lib/dashbord/draw_test.dart
@@ -1,51 +1,98 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:login_test/dashbord/test_screen.dart';
+import 'package:login_test/loading_screen/load_sscreen.dart';
+import 'package:login_test/loggin.dart';
+import 'package:login_test/terrr.dart';
+
+import 'home_drawer.dart';
 
 class CustomDraw extends StatefulWidget {
+  final AnimatedIconData animatedIconData;
+  final double drawerWidth;
+  final Function(bool) drawerIsOpen;
+
+  const CustomDraw(
+      {this.animatedIconData = AnimatedIcons.arrow_menu,
+      this.drawerWidth = 250,
+      this.drawerIsOpen});
+
   @override
   _CustomDrawState createState() => _CustomDrawState();
 }
 
-class _CustomDrawState extends State<CustomDraw>
-    with SingleTickerProviderStateMixin {
+class _CustomDrawState extends State<CustomDraw> with TickerProviderStateMixin {
   AnimationController _animationController;
+  AnimationController iconAnimationController;
+
+  // static ScrollController scrollController;
+  Widget screenView;
+  Widget menuView;
+  DrawerIndex drawerIndex;
+
 
   @override
   void initState() {
     super.initState();
     _animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+    screenView = LoginPage();
   }
 
   void toggle() => _animationController.isDismissed
       ? _animationController.forward()
       : _animationController.reverse();
 
-  var bigCont = Container(
-    color: Colors.amber,
-  );
-  var smallCont = Container(
-    color: Colors.blueGrey,
-  );
 
   @override
   Widget build(BuildContext context) {
-    final double maxSlide = 225.0;
+    final double maxSlide = 250.0;
     return GestureDetector(
-      onTap: toggle,
+      onHorizontalDragEnd: (dragEndDetails) {
+        if (dragEndDetails.primaryVelocity < 0)
+          _animationController.reverse();
+        else if (dragEndDetails.primaryVelocity > 0)
+          _animationController.forward();
+      },
       child: AnimatedBuilder(
         builder: (context, _) {
           double slide = maxSlide * _animationController.value;
           double scale = 1 - (_animationController.value * 0.3);
           return Stack(
             children: [
-              bigCont,
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (BuildContext context, Widget child) {
+                    return HomeDrawer(
+                      screenIndex: drawerIndex == null? DrawerIndex.HOME : drawerIndex,
+                      iconAnimationController: _animationController,
+                      callBackIndex: (DrawerIndex indexType) {
+                        try {
+                          changeIndex(indexType);
+                        } catch (e) {}
+                      },
+                    );
+                  },
+                ),
+              ),
               Transform(
                   transform: Matrix4.identity()
                     ..translate(slide)
                     ..scale(scale),
                   alignment: Alignment.centerLeft,
-                  child: smallCont)
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                        _animationController.isDismissed ? 0 : 30),
+                    child: Stack(
+                      children: [
+                        screenView,
+                        drawIcon(),
+                      ],
+                    ),
+                  ))
             ],
           );
         },
@@ -53,5 +100,57 @@ class _CustomDrawState extends State<CustomDraw>
         // child: smallCont,
       ),
     );
+  }
+
+  Widget drawIcon() {
+    return Padding(
+      padding:
+          EdgeInsets.only(top: MediaQuery.of(context).padding.top + 8, left: 8),
+      child: SizedBox(
+        width: AppBar().preferredSize.height - 8,
+        height: AppBar().preferredSize.height - 8,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(AppBar().preferredSize.height),
+            child: Center(
+              // if you use your own menu view UI you add form initialization
+              child: AnimatedIcon(
+                icon: AnimatedIcons.menu_arrow,
+                progress: _animationController,
+              ),
+            ),
+            onTap: () {
+              toggle();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      if (drawerIndex == DrawerIndex.HOME) {
+        setState(() {
+          screenView = LoginScreen3();
+        });
+      } else if (drawerIndex == DrawerIndex.Help) {
+        setState(() {
+          screenView = TestScreen();
+        });
+      } else if (drawerIndex == DrawerIndex.FeedBack) {
+        setState(() {
+          screenView = Container(color: Colors.yellow,);
+        });
+      } else if (drawerIndex == DrawerIndex.Invite) {
+        setState(() {
+          screenView = LoadingScreen();
+        });
+      } else {
+        //do in your way......
+      }
+    }
   }
 }
